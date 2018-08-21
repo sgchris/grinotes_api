f366fdbb30cd1e453e6b747693815b1d7da766ff
Gregory Chris <sgchris@gmail.com>
2018-08-21 11:45:52 +0300

    updated docs

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -18,6 +18,15 @@
                 margin: 0;
                 padding: 10px;
             }
+            .request-type {
+                font-weight: bold;
+                display: inline-block;
+                margin-right: 10px;
+            }
+            .request-get { color: darkgreen; }
+            .request-post { color: darkblue; }
+            .request-put { color: darkorange; }
+            .request-delete { color: darkred; }
         </style>
     </head>
     <body>
@@ -28,17 +37,54 @@
             Authentication token is in the headers:<br />
             access-token: {facebook-auth-token}
         </p>
+
+        <hr />
         
-        <h2>/files</h2>
+        <h2>Metadata</h2>
+        <h3>
+            <span class="request-type request-get">GET</span>
+            <span class="request-type request-post">POST</span> 
+            /metadata
+        </h3>
+        <p>Get or add metadata key-value pair</p>
+        
+        <h3>
+            <span class="request-type request-get">GET</span>
+            <span class="request-type request-put">PUT</span> 
+            <span class="request-type request-delete">DELETE</span> 
+            /metadata/{metadata-key}
+        </h3>
+        <p>Manage metadata single key-value pair</p>
+
+        <hr />
+        <h2>Files</h2>
+        <h3>
+            <span class="request-type request-get">GET</span>
+            <span class="request-type request-post">POST</span> 
+            /files
+        </h3>
         <p>All user files. params: limit (50), offset (0)</p>
-
-        <h2>/files/{file-id}</h2>
+        
+        <h3>
+            <span class="request-type request-get">GET</span>
+            <span class="request-type request-put">PUT</span>
+            <span class="request-type request-delete">DELETE</span>
+            /files/{file-id}
+        </h3>
         <p>User file data</p>
-
-        <h2>/files/{file-id}/versions</h2>
+        
+        <h3>
+            <span class="request-type request-get">GET</span>
+            <span class="request-type request-post">POST</span> 
+            /files/{file-id}/versions
+        </h3>
         <p>User file all versions. params: limit (20), offset (0)</p>
-
-        <h2>/files/{file-id}/versions/{version-id}</h2>
+        
+        <h3>
+            <span class="request-type request-get">GET</span>
+            <span class="request-type request-delete">DELETE</span>
+            /files/{file-id}/versions/{version-id}
+        </h3>
         <p>User file data at specific version</p>
     </body>
 </html>
